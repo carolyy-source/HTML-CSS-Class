eb98857cce1906ee985036cc7f75649ade793c10
Carole Yan <gill20519@gmail.com>
2025-11-24 19:53:30 +0800

    add JS file to switch the section

---
+++ Homework/eztravel.js
@@ -0,0 +1,14 @@
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".japan");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    // 清除所有 active 與 show
+    tabs.forEach((t) => t.classList.remove("active"));
+    contents.forEach((c) => c.classList.remove("show"));
+
+    // 加上目前的 active 與 show
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.target).classList.add("show");
+  });
+});
