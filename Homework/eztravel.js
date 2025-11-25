63c3731d5e0bac040ad3b7d5c4c5e9d36e72614f
carol <gill20519@gmail.com>
2025-11-25 17:09:36 +0800

    update section-tw part

--- Homework/eztravel.js
+++ Homework/eztravel.js
@@ -12,3 +12,18 @@ tabs.forEach((tab) => {
     document.getElementById(tab.dataset.target).classList.add("show");
   });
 });
+
+const TWtabs = document.querySelectorAll(".change");
+const TWcontents = document.querySelectorAll(".taiwan");
+
+TWtabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    // 清除所有 active 與 show
+    TWtabs.forEach((t) => t.classList.remove("active"));
+    TWcontents.forEach((c) => c.classList.remove("show"));
+
+    // 加上目前的 active 與 show
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.target).classList.add("show");
+  });
+});
